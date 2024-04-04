384163b4efbc61321154e066d0c73c7481f7481c
MDIVS <maiconoficialbr@gmail.com>
2024-04-04 11:10:37 -0300

    class 2 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Terminal } from "lucide-react";
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
 
 export function App() {
   return (
-    <div className="flex">
-      <Terminal /><h1>Hello World</h1>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
